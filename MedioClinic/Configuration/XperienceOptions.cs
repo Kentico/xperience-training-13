9796ab437e47bb4349341e07cf37dee48c58559a
Jan Lenoch <johnlazybones@outlook.com>
2020-05-12 12:30:24 +0200

    Resolves #24
    TRAIN-1625, TRAIN-1626, TRAIN-1627

--- MedioClinic/Configuration/XperienceOptions.cs
+++ MedioClinic/Configuration/XperienceOptions.cs
@@ -2,15 +2,15 @@
 {
     public class XperienceOptions
     {
-        public string SiteName { get; set; }
+        public string? SiteName { get; set; }
 
-        public MediaLibraryOptions MediaLibraryOptions { get; set; }
+        public MediaLibraryOptions? MediaLibraryOptions { get; set; }
     }
 
     public class MediaLibraryOptions
     {
-        public string[] AllowedImageExtensions { get; set; }
+        public string[]? AllowedImageExtensions { get; set; }
 
-        public string MedicalCentersLibrary { get; set; }
+        public string? MedicalCentersLibrary { get; set; }
     }
 }
