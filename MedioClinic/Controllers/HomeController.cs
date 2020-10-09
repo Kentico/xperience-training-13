3decc428af559c3581189f5f5700b118989147bb
Jan Lenoch <johnlazybones@outlook.com>
2020-10-09 13:43:33 +0200

    Remove unneeded code
    TRAIN-1625

--- MedioClinic/Controllers/HomeController.cs
+++ MedioClinic/Controllers/HomeController.cs
@@ -70,11 +70,5 @@ namespace MedioClinic.Controllers
         {
             return View();
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
