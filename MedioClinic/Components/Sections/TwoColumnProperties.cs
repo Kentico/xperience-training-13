e98a78ed8e7aaa3879fd584e246f58baf7916c60
Jan Lenoch <johnlazybones@outlook.com>
2021-05-10 15:55:10 +0200

    Resolves #69
    TRAIN-1627

---
+++ MedioClinic/Components/Sections/TwoColumnProperties.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Kentico.Forms.Web.Mvc;
+using Kentico.PageBuilder.Web.Mvc;
+
+namespace MedioClinic.Components.Sections
+{
+    public class TwoColumnProperties : ISectionProperties
+    {
+        [EditingComponent(
+            IntInputComponent.IDENTIFIER,
+            DefaultValue = 6,
+            Label = "{$" + ComponentIdentifiers.TwoColumnSection + ".LeftColumnWidth.Title$}",
+            ExplanationText = "{$" + ComponentIdentifiers.TwoColumnSection + ".LeftColumnWidth.Description$}",
+            Order = 0)]
+        public int LeftColumnWidth { get; set; }
+    }
+}
