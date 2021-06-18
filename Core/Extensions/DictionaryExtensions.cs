6e7a3f050b48aa9c791586437bbec5d4728c7e23
Jan Lenoch <johnlazybones@outlook.com>
2021-06-18 16:10:44 +0200

    Refactor HTML attribute manipulation
    TRAIN-1627

---
+++ Core/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Extensions
+{
+    public static class DictionaryExtensions
+    {
+        public static void AddOrEnrichEntry(this IDictionary<string, object> dictionary, string key, string value, char? separator = ' ')
+        {
+            if (!dictionary.TryAdd(key, value))
+            {
+                if (dictionary[key] is string)
+                {
+                    dictionary[key] += $"{separator}{value}";
+                }
+            }
+        }
+    }
+}
