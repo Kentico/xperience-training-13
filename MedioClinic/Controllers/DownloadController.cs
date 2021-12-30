b0543fd0c20bfd4ae3b8b453c93c5ad14939cc27
Jan Lenoch <johnlazybones@outlook.com>
2021-12-30 21:00:58 +0100

    Implement DownloadController
    TRAIN-1965

--- MedioClinic/Controllers/DownloadController.cs
+++ MedioClinic/Controllers/DownloadController.cs
@@ -1,9 +1,8 @@
 ﻿using CMS.Activities;
-using CMS.ContactManagement;
-using CMS.DataEngine;
 
 using Core.Configuration;
 
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Logging;
@@ -13,6 +12,8 @@ using Microsoft.Net.Http.Headers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -24,6 +25,8 @@ namespace MedioClinic.Controllers
 {
     public class DownloadController : BaseController
     {
+        private readonly IWebHostEnvironment _environment;
+
         private readonly IMediaFileRepository _mediaFileRepository;
 
         private readonly IActivityLogService _activityLogService;
@@ -32,11 +35,13 @@ namespace MedioClinic.Controllers
             ILogger<DownloadController> logger,
             IOptionsMonitor<XperienceOptions> optionsMonitor,
             IStringLocalizer<SharedResource> stringLocalizer,
+            IWebHostEnvironment environment,
             IMediaFileRepository mediaFileRepository,
             IActivityLogService activityLogService
             )
             : base(logger, optionsMonitor, stringLocalizer)
         {
+            _environment = environment ?? throw new ArgumentNullException(nameof(environment));
             _mediaFileRepository = mediaFileRepository ?? throw new ArgumentNullException(nameof(mediaFileRepository));
             _activityLogService = activityLogService ?? throw new ArgumentNullException(nameof(activityLogService));
         }
@@ -48,22 +53,41 @@ namespace MedioClinic.Controllers
                 return BadRequest();
             }
 
-            //var currentContact = ContactManagementContext.GetCurrentContact(true);
-
-            //var mediaFile = await _mediaFileRepository.GetMediaFileAsync(mediaLibraryId, path, cancellationToken);
             var mediaFile = await _mediaFileRepository.GetMediaFileAsync(fileGuid.Value, cancellationToken);
 
             if (mediaFile != null)
             {
                 var initializer = new FileDownloadActivityInitializer(DownloadType.Public, mediaFile.MediaFileUrl?.RelativePath!);
                 _activityLogService.Log(initializer);
+                var etagValue = mediaFile.LastModified?.DateTime.ToString() ?? mediaFile.GetHashCode().ToString();
+                var hashedEtagValue = GetHash(etagValue!);
+                var etag = new EntityTagHeaderValue($"\"{hashedEtagValue}\"", false);
+                var path = mediaFile.MediaFileUrl?.DirectPath.Trim('~').Replace('/', '\\');
+                var completePath = $"{_environment.ContentRootPath}{path}";
 
-                var etag = new EntityTagHeaderValue(mediaFile.LastModified?.DateTime.ToString(), false);
+                // ControllerBase.File() takes care of the object disposal, hence no 'using' statement.
+                var stream = System.IO.File.OpenRead(completePath);
 
-                return File(mediaFile.MediaFileUrl?.RelativePath, mediaFile.MimeType, mediaFile.LastModified, etag);
+                return File(stream, mediaFile.MimeType, mediaFile.LastModified, etag);
             }
 
             return NotFound();
+        }
+
+        private static string GetHash(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return null!;
+            }
+
+            using (var sha256 = SHA256.Create())
+            {
+                var bytes = Encoding.UTF8.GetBytes(input);
+                var hashedBytes = sha256.ComputeHash(bytes);
+
+                return BitConverter.ToString(hashedBytes).Replace("-", string.Empty);
+            }
         }
     }
 }
